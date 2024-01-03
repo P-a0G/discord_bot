ca6fea09ddc166aa2397dd1a2421a271732f4df1
Flash <Flash@mailtest.com>
2024-01-03 17:13:23 +0100

    anniv save

--- discord_bot.py
+++ discord_bot.py
@@ -1,5 +1,8 @@
+import os
+
 import discord
-from utils import is_valid_url, extract_from_url, get_size, read_json
+from utils import is_valid_url, extract_from_url, get_size, read_json, write_json
+import datetime
 
 intents = discord.Intents.default()
 intents.members = True
@@ -7,7 +10,11 @@ intents.message_content = True
 
 client = discord.Client(intents=intents)
 
-anniversaries = {}
+anniversary_dict_path = "anniversary.json"
+if os.path.exists(anniversary_dict_path):
+    anniversaries = read_json(anniversary_dict_path)
+else:
+    anniversaries = dict()
 
 
 @client.event
@@ -17,14 +24,41 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    print(message)
-    print("id:", message.id)
-    print("channel:", message.channel)
-    print("type:", message.type)
-    print("author:", message.author)
-    print("content:", message.content)
+    # print("id:", message.id)
+    # print("channel:", message.channel)
+    # print("type:", message.type)
+    # print("author:", message.author)
+    # print("content:", message.content)
 
-    if not (message.channel == "Direct Message with Unknown User" or "bot" in message.channel):
+    if message.content.startswith("set birthday"):
+        _, _, anniversary, user = message.content.split(" ")
+        try:
+            day, mounth, year = anniversary.split("/")
+            day = int(day)
+            mounth = int(mounth)
+            year = int(year)
+
+        except ValueError:
+            await message.channel.send("Format invalide, utiliser: '!set birthday DD/MM/YYYY User'.")
+            return
+
+        if not 0 < day < 32:
+            await message.channel.send("Mettre la date au format: DD/MM/YYYY")
+            return
+        if not 0 < mounth < 13:
+            await message.channel.send("Mettre la date au format: DD/MM/YYYY")
+            return
+
+        anniversaries[user] = {
+            "day": day,
+            "mounth": mounth,
+            "year": year
+        }
+        write_json(anniversary_dict_path, anniversaries)
+        await message.channel.send("Saved")
+        return
+
+    if not (str(message.channel) == "Direct Message with Unknown User" or "bot" in str(message.channel)):
         return
     
     if message.author == client.user:
