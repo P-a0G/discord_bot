923167cc6f4c5d7ec7e49fa175829fd3ad378a69
Flash <Flash@mailtest.com>
2024-02-21 14:46:11 +0100

    clear code

--- modules/music_utils.py
+++ modules/music_utils.py
@@ -9,7 +9,7 @@ import eyed3
 from eyed3.id3.frames import ImageFrame
 
 
-def extract_from_url(url, output_dir=r"musics/", add_tags=True):
+def extract_with_bs(url):
     # Step 2: Send an HTTP request to the page
     response = requests.get(url)
     html_content = response.text
@@ -20,17 +20,18 @@ def extract_from_url(url, output_dir=r"musics/", add_tags=True):
     # Get the video title
     title = soup.find("title").text.strip()
 
-    if not os.path.exists(output_dir):
-        os.mkdir(output_dir)
+    return title, soup
 
+
+def get_metadata(title):
     # getting title of the film and author name from YT video title
     author = None
     if "-" in title:
         author, title = title.split("-")[:2]
         title = title.strip()
         author = author.strip()
-    print("author:", author)
-    print("title:", title)
+    print("\tauthor:", author)
+    print("\ttitle:", title)
 
     if title.lower() == "youtube":
         title = author.split(" (")[0]
@@ -44,6 +45,10 @@ def extract_from_url(url, output_dir=r"musics/", add_tags=True):
     title = title.strip()
     filename = title + ".webm"
 
+    return title, author, filename
+
+
+def get_music_with_pytube(url, audio_file_path):
     yt = YouTube(url)
 
     # Step 2: Get the audio streams
@@ -51,51 +56,72 @@ def extract_from_url(url, output_dir=r"musics/", add_tags=True):
         audio_streams = yt.streams.filter(only_audio=True, mime_type="audio/webm")
     except:
         logging.info(f' >> Error trying to get audio_streams')
-        audio_streams = None
+        return 0
 
     if audio_streams:
         best_audio = audio_streams[-1]
 
-        audio_file_path = os.path.join(output_dir, filename)
         best_audio.download(filename=audio_file_path)
 
         logging.info(f' >> File saved in {audio_file_path}')
 
-    else:
+    return 1
 
-        # Get the download URL for the MP3 file
-        download_url = None
 
-        for script in soup.find_all("script"):
-            script_text = str(urllib.parse.unquote(script.text)).replace("\\u0026", "&")
-            if "audioQualityLabels" in script_text:
-                download_url = script_text.split("url\":\"")[1].split("\",\"")[0].replace("\\u0026", "&")
-                break
+def get_music_with_bs(soup, audio_file_path):
+    # Get the download URL for the MP3 file
+    download_url = None
 
-            if "audioquality" in script_text.lower():
+    for script in soup.find_all("script"):
+        script_text = str(urllib.parse.unquote(script.text)).replace("\\u0026", "&")
+        if "audioQualityLabels" in script_text:
+            download_url = script_text.split("url\":\"")[1].split("\",\"")[0].replace("\\u0026", "&")
+            break
 
-                if "flashUrl" not in script_text:
-                    continue
+        if "audioquality" in script_text.lower():
 
-                download_url = script.text.split("flashUrl\":\"")[1].split("\",\"")[0]
-                break
+            if "flashUrl" not in script_text:
+                continue
 
-        if download_url is None:
-            logging.error("[Error] Didn't find download url")
-            return None
+            download_url = script.text.split("flashUrl\":\"")[1].split("\",\"")[0]
+            break
 
-        logging.info(f" > download url: {download_url}")
+    if download_url is None:
+        logging.error("[Error] Didn't find download url")
+        return None
 
-        yt = YouTube(download_url)
+    logging.info(f" > download url: {download_url}")
 
-        yt.streams.filter(only_audio=True)[0].download(output_path=output_dir, filename=filename)
+    yt = YouTube(download_url)
 
-        audio_file_path = os.path.join(output_dir, filename)
+    yt.streams.filter(only_audio=True)[0].download(filename=audio_file_path)
 
-        logging.info(f' >> File saved in {os.path.join(output_dir, filename)}')
+    logging.info(f' >> File saved in {audio_file_path}')
 
-        image_url = soup.find("meta", property="og:image")["content"]
-        print("image url:", image_url)
+    image_url = soup.find("meta", property="og:image")["content"]
+    print("image url:", image_url)
+
+    return audio_file_path
+
+
+def extract_from_url(url, output_dir=r"musics/", add_tags=True):
+    title, soup = extract_with_bs(url)
+
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
+
+    title, author, filename = get_metadata(title)
+
+    audio_file_path = os.path.join(output_dir, filename)
+
+    got_music = get_music_with_pytube(url, audio_file_path)
+
+    if not got_music:
+        got_music = get_music_with_bs(soup, audio_file_path)
+
+    if not got_music:
+        print("[Error] couldn't get the music")
+        return None
 
     try:
         output_path = audio_file_path.replace(".webm", ".mp3")
