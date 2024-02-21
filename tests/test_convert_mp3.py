b4ddac87acf59a2fb7080a41ddb513d478366970
Flash <Flash@mailtest.com>
2024-02-21 14:31:17 +0100

    init tests

---
+++ tests/test_convert_mp3.py
@@ -0,0 +1,27 @@
+import pytest
+from context import blabla  # todo
+
+
+def test_get_music():  # todo
+    assert True
+
+
+def test_get_metadata():  # todo
+    assert True
+
+
+def test_get_img_for_music():  # todo
+    assert True
+
+
+def test_convert_mp3():  # todo
+    assert True
+
+
+def test_set_metadata():  # todo
+    assert True
+
+
+def test_set_img():  # todo
+    assert True
+
