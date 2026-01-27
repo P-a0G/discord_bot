23b20524ace47ec414846c89366725dad453b153
PA Grosselin <pierre-antoine.grosselin@drotek.com>
2026-01-27 09:47:56 +0100

    fix: wrong order in history games

--- modules/riot_tracker/chore.py
+++ modules/riot_tracker/chore.py
@@ -68,7 +68,7 @@ def get_full_data_history(discord_users, riot_client, discord_id, last: int = 10
                 (account.puuid, riot_client.get_match(match_id))
             )
 
-    matchs.sort(key=lambda x: x["info"]["gameStartTimestamp"], reverse=True)
+    matchs.sort(key=lambda x: x["info"]["gameStartTimestamp"], reverse=False)
 
     return matchs
 
